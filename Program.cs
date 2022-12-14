d4df12813ab2694581fde81df71104e6feea0155
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-12-14 16:09:52 -0500

    investigating youtube pipeline

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
-﻿await Bootstrapper
+﻿using Statiq.YouTube;
+const string keyName = "GOOGLEAPIKEY";
+string value = "";
+
+await Bootstrapper
   .Factory
   .CreateWeb(args)
-  .RunAsync();
+  // .BuildPipeline("YouTube", builder =>
+  // {
+  //    var configApiKey = Config.FromSetting<string>(keyName).ThrowIfNull(keyName);
+  //    builder.WithInputConfig(configApiKey);
+  //    string apiKey = builder.Settings[keyName]?.ToString() ?? "";
 
+  //    builder.WithProcessModules();
+
+  //    var readYouTube = new ReadYouTube(apiKey)
+  //        .WithRequest(apiKey,
+  //            (_, service) => service.Videos.List("title,description,url"));
+
+  //    builder.WithInputModules(readYouTube);
+  //    builder.WithOutputWriteFiles();
+  // })
+  .RunAsync();
